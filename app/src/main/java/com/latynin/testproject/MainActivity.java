eca8a4516042c7892ba02d23984deba4ef261da7
LatyninEugene <31857296+LatyninEugene@users.noreply.github.com>
2017-10-31 21:15:29 +0300

    Login ready

--- app/src/main/java/com/latynin/testproject/MainActivity.java
+++ app/src/main/java/com/latynin/testproject/MainActivity.java
@@ -4,6 +4,8 @@ import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
